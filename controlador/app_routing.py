b3a369e06daa36e87a4225439ca354a4cbde623e
Erick Diaz Wences <erickcuate1@gmail.com>
2021-10-07 14:04:23 -0500

    Inicio de Proyecto

---
+++ controlador/app_routing.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__,template_folder='../paginas')
+
+@app.route('/')
+def incio():    
+    return  render_template('inicio/inicio.html')
+
+@app.route('/tarjetas')
+def tarjetas():    
+    return  render_template('tarjetas/tarjetas.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
